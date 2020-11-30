48e4edab0ea79de255b6f98564fa668692adda7f
sos <robinson1992818@outlook.com>
2020-11-30 06:12:23 -0900

    Demo app

---
+++ vue-app/demo/src/components/Posts.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class = "container">
+        <table class="table mt-4">
+            <thead>
+                <tr>
+                <th scope="col">userId</th>
+                <th scope="col">Id</th>
+                <th scope="col">Title</th>
+                <th scope="col">Completed</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="post in posts" :key ="post.id" >
+                    <td>{{post.id}}</td>
+                    <td>{{post.title}}</td>
+                    <td>{{post.completed}}</td>
+                </tr>
+            </tbody>
+            </table>
+    </div>
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+    name : 'PostsExample',
+    props : [
+        'posts'
+    ],
+    data() {
+        return {
+
+        }
+    },
+    created() {
+
+        axios.get('https://jsonplaceholder.typicode.com/todos').then( posts =>{
+            this.posts = posts.data;
+        })
+    }
+}
+</script>
---
+++ vue-app/demo/src/main.js
@@ -0,0 +1,31 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router'
+import Posts from './components/Posts'
+
+Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path:'/',
+    component : Posts,
+  }
+]
+
+const router = new VueRouter({
+  routes:routes
+})
+Vue.config.productTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { 
+    App,
+   },
+  template: '<App/>'
+})
